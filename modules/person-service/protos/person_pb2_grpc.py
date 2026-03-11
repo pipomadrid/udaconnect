d61daa8dcb34f41e3662f2a355dd43baa6cd4e94
Ubuntu <udacity@UbuntuVM-296596.2mufjgeh1rlepc1qmacoku3z3e.frax.internal.cloudapp.net>
2026-03-11 09:07:16 +0000

    pb2

--- modules/person-service/protos/person_pb2_grpc.py
+++ modules/person-service/protos/person_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from .protos import person_pb2 as person__pb2
+from protos import person_pb2 as person__pb2
 
 GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
