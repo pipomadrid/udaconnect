403557b407a775e399d31fd22ddd6ba0086c39a5
Sáez Perez <pedro.j.saez@accenture.com>
2026-03-11 12:51:49 +0100

    init

--- modules/person-service/app/person/__init__.py
+++ modules/person-service/app/person/__init__.py
@@ -3,6 +3,6 @@ from app.person.schemas import PersonSchema  # noqa
 
 
 def register_routes(api, app, root="api"):
-    from app.person import api as udaconnect_api
+    from app.person.controllers import api as udaconnect_person
 
-    api.add_namespace(udaconnect_api, path=f"/{root}")
+    api.add_namespace(udaconnect_person, path=f"/{root}")
